b144e595a6241ef28abb0a0b26260f0eea75f5c0
Jose-Luis Rivas <me@ghostbar.co>
2016-03-30 11:43:40 -0500

    my head still does not accepts my main machine is darwin, not linux

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,11 +8,11 @@ if [ -d build ]; then
 else
   mkdir build
 fi
-go build
-mv $BIN_NAME build/"$BIN_NAME"_linux_x86_64
+env GOOS=darwin GOARCH=amd64 go build
+mv $BIN_NAME build/"$BIN_NAME"_darwin_x86_64
 
 env GOOS=linux GOARCH=amd64 go build
-mv $BIN_NAME build/"$BIN_NAME"_darwin_x86_64
+mv $BIN_NAME build/"$BIN_NAME"_linux_x86_64
 
 cd build && shasum -a 512 "$BIN_NAME"_*_x86_64 > SHA512SUMS.txt && cd .. || exit 
 gpg --clearsign build/SHA512SUMS.txt
