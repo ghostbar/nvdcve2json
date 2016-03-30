f60a95c990928caf018358753980089a9162a062
Jose-Luis Rivas <me@ghostbar.co>
2016-03-30 12:03:54 -0500

    improve scripts/build.sh for as many targets as required

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,17 +2,20 @@
 # vim: ft=sh tw=80
 
 BIN_NAME="nvdcve2json"
+TARGETS="linux;amd64 linux;arm darwin;amd64"
 
 if [ -d build ]; then
   rm build/*
 else
   mkdir build
 fi
-env GOOS=darwin GOARCH=amd64 go build
-mv $BIN_NAME build/"$BIN_NAME"_darwin_x86_64
 
-env GOOS=linux GOARCH=amd64 go build
-mv $BIN_NAME build/"$BIN_NAME"_linux_x86_64
+for target in $TARGETS; do
+  targetos=$(echo $target | awk -F ";" '{ print $1 }')
+  targetarch=$(echo $target | awk -F ";" '{ print $2 }')
+  env GOOS=$targetos GOARCH=$targetarch go build
+  mv $BIN_NAME build/"$BIN_NAME"_"$targetos"_"$targetarch"
+done
 
-cd build && shasum -a 512 "$BIN_NAME"_*_x86_64 > SHA512SUMS.txt && cd .. || exit 
+cd build && shasum -a 512 "$BIN_NAME"_* > SHA512SUMS.txt && cd .. || exit 
 gpg --clearsign build/SHA512SUMS.txt
